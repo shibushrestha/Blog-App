06fed5382c70e3b74d1f66202253a168fad1dd84
shibushrestha <shibushrestha12@gmail.com>
2023-02-16 14:19:18 +0545

    urls are now all re_path() instances

--- project/Blog/urls.py
+++ project/Blog/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path, re_path
 
-from . import views
-
+from Blog import views
+from Blog import auth_views
 
 app_name = "Blog"
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('register/', views.register_user, name="register"),
-    path('login/', views.user_login, name="login"),
-    path('logout/', views.user_logout, name="logout"),
-    path('create/', views.create_post, name="create"),
-    path('post_detail/<slug:post_slug>/', views.post_detail, name='detail'),
-    path('<int:post_id>/delete/', views.delete_post, name="delete_post"),
-    path('<str:user_username>/', views.user_profile, name="profile"),
-    path('<str:user_username>/profileupdate/', views.update_user_profile, name="updateprofile")
+    re_path(r'^$', views.home, name="home"),
+    re_path(r'^post_detail/(?P<post_slug>[a-zA-Z0-9-]+)/?$', views.post_detail, name='detail'),
+
+    re_path(r'^register/$', auth_views.register_user, name="register"),
+    re_path(r'^login/$', auth_views.user_login, name="login"),
+    re_path(r'^logout/$', auth_views.user_logout, name="logout"),
+    re_path(r'^(?P<user_username>[a-zA-Z0-9]+)/?$', auth_views.user_profile, name="profile"),
+    re_path(r'^(?P<user_username>[a-zA-Z0-9]+)/profileupdate/', auth_views.update_user_profile, name="updateprofile"),
+
+    re_path(r'^create/$', views.create_post, name="create"),
+    re_path(r'^(?P<post_id>[\d]+)/?delete/$', views.delete_post, name="delete_post"),
+
     
 ]
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,18 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Blog/', include('Blog.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^Blog/', include('Blog.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 
 
